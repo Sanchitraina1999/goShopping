b87e7bfc9a2b863fa37ecf4042ebfd699f0b9888
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-12 17:32:44 +0530

    browserRouter added :white_check_mark:

--- goshopping/src/App.js
+++ goshopping/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <BrowserRouter>
+      <div className="App">Hi</div>
+    </BrowserRouter>
   );
 }
 
