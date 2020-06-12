f3392835be1740f6d109729cd90c82a3156f6145
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-12 21:49:00 +0530

    projectSummary Component  added :tada:

--- goshopping/src/components/items/ItemList.js
+++ goshopping/src/components/items/ItemList.js
@@ -1,31 +1,13 @@
 import React from "react";
+import ItemSummary from "./ItemSummary";
 
 const ItemList = () => {
   return (
     <div className="Item-list section">
-      <div className="card z-depth-0 ItemSummary">
-        <div className="card-content grey-text text-darken0-3">
-          <span className="card-title">Item Title</span>
-          <p>Posted by Sanchit</p>
-          <p className="grey-text">12 June, 2020 7pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 ItemSummary">
-        <div className="card-content grey-text text-darken0-3">
-          <span className="card-title">Item Title</span>
-          <p>Posted by Sanchit</p>
-          <p className="grey-text">12 June, 2020 7pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 ItemSummary">
-        <div className="card-content grey-text text-darken0-3">
-          <span className="card-title">Item Title</span>
-          <p>Posted by Sanchit</p>
-          <p className="grey-text">12 June, 2020 7pm</p>
-        </div>
-      </div>
+      <ItemSummary />
+      <ItemSummary />
+      <ItemSummary />
+      <ItemSummary />
     </div>
   );
 };
---
+++ goshopping/src/components/items/ItemSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ItemSummary = () => {
+  return (
+    <div className="card z-depth-0 ItemSummary">
+      <div className="card-content grey-text text-darken0-3">
+        <span className="card-title">Item Title</span>
+        <p>Posted by Sanchit</p>
+        <p className="grey-text">12 June, 2020 7pm</p>
+      </div>
+    </div>
+  );
+};
+
+export default ItemSummary;
