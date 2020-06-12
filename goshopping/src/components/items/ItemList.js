1e61bab5dd4bc68a0035d58b425703ae9ee9c802
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-12 19:54:27 +0530

    Dashboard Component added :tada:

---
+++ goshopping/src/components/items/ItemList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ItemList = () => {
+  return (
+    <div className="Item-list section">
+      <div className="card z-depth-0 ItemSummary">
+        <div className="card-content grey-text text-darken0-3">
+          <span className="card-title">Item Title</span>
+          <p>Posted by Sanchit</p>
+          <p className="grey-text">12 June, 2020 7pm</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 ItemSummary">
+        <div className="card-content grey-text text-darken0-3">
+          <span className="card-title">Item Title</span>
+          <p>Posted by Sanchit</p>
+          <p className="grey-text">12 June, 2020 7pm</p>
+        </div>
+      </div>
+
+      <div className="card z-depth-0 ItemSummary">
+        <div className="card-content grey-text text-darken0-3">
+          <span className="card-title">Item Title</span>
+          <p>Posted by Sanchit</p>
+          <p className="grey-text">12 June, 2020 7pm</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
