ef2d5b23a2e26abf7bc023dc7521983c0f51a03b
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-13 12:19:49 +0530

    firebaseConfig added :white_check_mark:

---
+++ goshopping/src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCc0PxMobOFEt_-gjQiT7q9UtqXAlk2x9Q",
+    authDomain: "sanchit199-goshopping.firebaseapp.com",
+    databaseURL: "https://sanchit199-goshopping.firebaseio.com",
+    projectId: "sanchit199-goshopping",
+    storageBucket: "sanchit199-goshopping.appspot.com",
+    messagingSenderId: "200880810386",
+    appId: "1:200880810386:web:e64b84a4595a874d11394b",
+    measurementId: "G-6XQNW7DBS4"
+  };
