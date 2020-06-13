1be025571f45dfaef56b0accc565b28e9ae00fbe
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-13 13:06:51 +0530

    redux connected to firebase

--- goshopping/src/store/actions/itemActions.js
+++ goshopping/src/store/actions/itemActions.js
@@ -1,5 +1,5 @@
 export const createItem = (item) => {
-  return (dispatch, getState,{getFirebase,getFirestore}) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
     dispatch({ type: "CREATE_ITEM", item });
   };
