889f1e663c23146f8b420e71676da0bd88731234
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-13 12:26:30 +0530

    firebase added :fire:

--- goshopping/src/config/firebaseConfig.js
+++ goshopping/src/config/firebaseConfig.js
@@ -1,10 +1,20 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCc0PxMobOFEt_-gjQiT7q9UtqXAlk2x9Q",
-    authDomain: "sanchit199-goshopping.firebaseapp.com",
-    databaseURL: "https://sanchit199-goshopping.firebaseio.com",
-    projectId: "sanchit199-goshopping",
-    storageBucket: "sanchit199-goshopping.appspot.com",
-    messagingSenderId: "200880810386",
-    appId: "1:200880810386:web:e64b84a4595a874d11394b",
-    measurementId: "G-6XQNW7DBS4"
-  };
+import firebase from "firebase/app";
+import "firebase/firebase";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCc0PxMobOFEt_-gjQiT7q9UtqXAlk2x9Q",
+  authDomain: "sanchit199-goshopping.firebaseapp.com",
+  databaseURL: "https://sanchit199-goshopping.firebaseio.com",
+  projectId: "sanchit199-goshopping",
+  storageBucket: "sanchit199-goshopping.appspot.com",
+  messagingSenderId: "200880810386",
+  appId: "1:200880810386:web:e64b84a4595a874d11394b",
+  measurementId: "G-6XQNW7DBS4",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
