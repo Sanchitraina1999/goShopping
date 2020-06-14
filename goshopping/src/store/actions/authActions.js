304dceb4700af6aeaa316a72d6378fb2114575dc
Sanchitraina1999 <sanchitraina1999@gmail.com>
2020-06-15 01:06:46 +0530

    userSignUpWithFirebaseAuth done

--- goshopping/src/store/actions/authActions.js
+++ goshopping/src/store/actions/authActions.js
@@ -25,3 +25,29 @@ export const signOut = () => {
       });
   };
 };
+
+export const signUp = (newUser) => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firebase = getFirebase();
+    const firestore = getFirestore();
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+      .then((resp) => {
+        return firestore
+          .collection("users")
+          .doc(resp.user.uid)
+          .set({
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            initials: newUser.firstName[0] + newUser.lastName[0],
+          });
+      })
+      .then(() => {
+        dispatch({ type: "SIGN_UP_SUCCESS" });
+      })
+      .catch((err) => {
+        dispatch({ type: "SIGN_UP_ERROR", err });
+      });
+  };
+};
--- goshopping/src/store/reducers/authReducer.js
+++ goshopping/src/store/reducers/authReducer.js
@@ -17,7 +17,20 @@ const authReducer = (state = initState, action) => {
         authError: null,
       };
     case "LOG_OUT_SUCCESS":
+      console.log("LogOut Success");
       return state;
+    case "SIGN_UP_SUCCESS":
+      console.log("SignUp Success");
+      return {
+        ...state,
+        authError: null,
+      };
+    case "SIGN_UP_ERROR":
+      console.log("Sign Up Error");
+      return {
+        ...state,
+        authError: action.err.message,
+      };
     default:
       return state;
   }
